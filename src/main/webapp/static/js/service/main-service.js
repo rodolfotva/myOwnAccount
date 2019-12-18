07ca39ebce721b046aedeea0e04aa5d0c7716f4f
Rodolfo Alquezar <rodolfotva@gmail.com>
2019-12-18 13:45:21 -0500

    change url for server

--- src/main/webapp/static/js/service/main-service.js
+++ src/main/webapp/static/js/service/main-service.js
@@ -1,13 +1,13 @@
 angular.module('main').factory('mainService', ['$http', '$q', function($http, $q){
 
 	var UAU = 'http://localhost:8082/myOwnAccount';
-    var REST_SERVICE_DATA_URL = UAU + '/header/';
-    var REST_SERVICE_DOLOGIN_URL = UAU + '/user/login/';
-    var REST_SERVICE_ACCOUNTS_BYUSER_URL = UAU + '/account/byuser/';
-    var REST_SERVICE_LINEITENS_BYACC_URL = UAU + '/lineitem/byaccount/';
-    var REST_SERVICE_LOGOUT_URL = UAU + '/user/logout/';
-    var REST_SERVICE_ADD_ACCOUNT = UAU + '/account/add/';
-    var REST_SERVICE_ADD_LINEITEM = UAU + '/lineitem/add/';
+    var REST_SERVICE_DATA_URL = '/header/';
+    var REST_SERVICE_DOLOGIN_URL = '/user/login/';
+    var REST_SERVICE_ACCOUNTS_BYUSER_URL = '/account/byuser/';
+    var REST_SERVICE_LINEITENS_BYACC_URL = '/lineitem/byaccount/';
+    var REST_SERVICE_LOGOUT_URL = '/user/logout/';
+    var REST_SERVICE_ADD_ACCOUNT = '/account/add/';
+    var REST_SERVICE_ADD_LINEITEM = '/lineitem/add/';
     
     var factory = {
    		fetchAllData:fetchAllData,
